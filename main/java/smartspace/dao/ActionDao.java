37050398d7712e80a37e53844015d3c838b9b675
AmitPelerman <amitpelerman@gmail.com>
2019-03-13 22:46:56 +0200

    Fixed bugs in Dao MemoryUserDao

--- main/java/smartspace/dao/ActionDao.java
+++ main/java/smartspace/dao/ActionDao.java
@@ -6,7 +6,7 @@ import smartspace.data.ActionEntity;;
 //Amit 13\03
 public interface ActionDao {
 	public ActionEntity create(ActionEntity actionEntity);
-	public List<ActionEntity> readAll(ActionEntity actionEntity);	
+	public List<ActionEntity> readAll();	
 	public void deleteAll();
 }
 //
--- main/java/smartspace/dao/MemoryUserDao.java
+++ main/java/smartspace/dao/MemoryUserDao.java
@@ -3,38 +3,74 @@ package smartspace.dao;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import smartspace.data.UserEntity;
 
-public class MemoryUserDao implements UserDao<UserKey> {
+//Amit 13/03
+@Repository
+public class MemoryUserDao implements UserDao<String> {
+	private List<UserEntity> users;
 
+	// To Do
 	@Override
 	public UserEntity create(UserEntity userEntity) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<UserEntity> readAll(UserEntity userEntity) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<UserEntity> readAll() {
+		return this.users;
 	}
 
 	@Override
-	public Optional<UserEntity> readbyId(UserKey userKey) {
-		// TODO Auto-generated method stub
-		return null;
+	public Optional<UserEntity> readById(String userKey) {
+		UserEntity target = null;
+		for (UserEntity current : this.users) {
+			if (current.getKey().equals(userKey)) {
+				target = current;
+			}
+		}
+		if (target != null) {
+			return Optional.of(target);
+		} else {
+			return Optional.empty();
+		}
 	}
 
+//Amit - Have to decide about line 66
 	@Override
-	public void update(UserEntity userEntity) {
-		// TODO Auto-generated method stub
+	public void update(UserEntity update) {
+		synchronized (this.users) {
+			UserEntity existing = this.readById(update.getKey())
+					.orElseThrow(() -> new RuntimeException("not message to update"));
+			if (update.getAvatar() != null) {
+				existing.setAvatar(update.getAvatar());
+			}
+			if (update.getUserEmail() != null) {
+				existing.setUserEmail(update.getUserEmail());
+			}
+			if (update.getUsername() != null) {
+				existing.setUsername(update.getUsername());
+			}
+			if (update.getUserSmartspace() != null) {
+				existing.setUserSmartspace(update.getUserSmartspace());
+			}
+			if (update.getRole() != null) {
+				existing.setRole(update.getRole());
+			}
+			// ????????
+//			if (update.getPoints() >= 0) {
+//				existing.setPoints(update.getPoints());
+//			}
 
+		}
 	}
 
 	@Override
 	public void deleteAll() {
-		// TODO Auto-generated method stub
-
+		this.users.clear();
 	}
 
 }
+//
