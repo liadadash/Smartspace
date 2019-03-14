7e048aa3c8f25b2ad38dd548c642a7fb291f3e28
LiadKhamd <liadkh95@gmail.com>
2019-03-14 19:45:41 +0200

    Create memory action dao

---
+++ main/java/smartspace/dao/memory/MemoryActionDao.java
@@ -0,0 +1,44 @@
+package smartspace.dao.memory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import smartspace.dao.ActionDao;
+import smartspace.data.ActionEntity;
+
+/**
+ * @author liadk
+ *
+ */
+
+public class MemoryActionDao implements ActionDao {
+
+	private List<ActionEntity> actions;
+	private AtomicLong nextId;
+
+	public MemoryActionDao() {
+		this.actions = Collections.synchronizedList(new ArrayList<>());
+		this.nextId = new AtomicLong(1);
+	}
+
+	@Override
+	public ActionEntity create(ActionEntity actionEntity) {
+		actionEntity.setKey(actionEntity.getActionSmartspace() + actionEntity.getElementId()
+				+ actionEntity.getActionType() + "" + nextId.getAndIncrement());
+		this.actions.add(actionEntity);
+		return actionEntity;
+	}
+
+	@Override
+	public List<ActionEntity> readAll() {
+		return this.actions;
+	}
+
+	@Override
+	public void deleteAll() {
+		this.actions.clear();
+	}
+
+}
