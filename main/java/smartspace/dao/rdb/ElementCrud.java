cde83e07be9a812b13ee8a40ead20e36842ed7f3
Liad Khamdadash <liadkh95@gmail.com>
2019-05-08 20:14:46 +0300

    Manager or player get elements - DONE!

--- main/java/smartspace/dao/rdb/ElementCrud.java
+++ main/java/smartspace/dao/rdb/ElementCrud.java
@@ -1,11 +1,66 @@
 package smartspace.dao.rdb;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import smartspace.data.ElementEntity;
 import smartspace.data.ElementKey;
 
+/**
+ * The Interface ElementCrud.
+ * @author liadkh
+ */
 public interface ElementCrud extends
 //CrudRepository<ElementEntity, ElementKey>{
 		PagingAndSortingRepository<ElementEntity, ElementKey> {
+
+	/**
+	 * Find all by expired.
+	 *
+	 * @param expired the expired
+	 * @param pageable the pageable
+	 * @return the list
+	 */
+	public List<ElementEntity> findAllByExpired(boolean expired, Pageable pageable);
+
+	/**
+	 * Find all by name.
+	 *
+	 * @param name the name
+	 * @param pageable the pageable
+	 * @return the list
+	 */
+	public List<ElementEntity> findAllByName(String name, Pageable pageable);
+	
+	/**
+	 * Find all by type.
+	 *
+	 * @param type the type
+	 * @param pageable the pageable
+	 * @return the list
+	 */
+	public List<ElementEntity> findAllByType(String type, Pageable pageable);
+	
+	/**
+	 * Find all by name and expired.
+	 *
+	 * @param name the name
+	 * @param expired the expired
+	 * @param pageable the pageable
+	 * @return the list
+	 */
+	public List<ElementEntity> findAllByNameAndExpired(String name, boolean expired, Pageable pageable);
+
+	/**
+	 * Find all by type and expired.
+	 *
+	 * @param type the type
+	 * @param expired the expired
+	 * @param pageable the pageable
+	 * @return the list
+	 */
+	public List<ElementEntity> findAllByTypeAndExpired(String type, boolean expired, Pageable pageable);
+
 }
