274b5e53c2a16b337b4d685b8e4556ce7b690d44
Amit Pelerman <amitpelerman@gmail.com>
2019-05-12 15:20:05 +0000

    unnecessary class(duplicated)

--- main/java/smartspace/infra/updateElementService.java
+++
@@ -1,8 +0,0 @@
-package smartspace.infra;
-
-import smartspace.data.ElementEntity;
-
-public interface UpdateElementService {
-
-	public void updateElement(String managerSmartspace , String managerEmail ,ElementEntity elementEntity);
-}
