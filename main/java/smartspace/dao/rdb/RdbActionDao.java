d2b0e61cf4f3fdd837579e947844787eef4cfd3c
Nadav42 <bscpkd@Gmail.com>
2019-05-25 01:37:44 +0300

    fixed two tests but @transactional doesnt work with mongo

--- main/java/smartspace/dao/rdb/RdbActionDao.java
+++ main/java/smartspace/dao/rdb/RdbActionDao.java
@@ -2,7 +2,6 @@ package smartspace.dao.rdb;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
