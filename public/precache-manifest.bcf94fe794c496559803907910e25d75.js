4aed5fe3a3bb3f3b45499e5dd4543177f430f470
Liad Khamdadash <liadkh95@gmail.com>
2019-06-02 13:24:11 +0300

    Project conf.

---
+++ public/precache-manifest.bcf94fe794c496559803907910e25d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db17354377a1d8e71b047fdcb4ffd590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dde82a8e39178cd256d",
+    "url": "/static/css/2.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "c1b3569d5b2f6f15fd9a",
+    "url": "/static/css/main.00534bb2.chunk.css"
+  },
+  {
+    "revision": "6dde82a8e39178cd256d",
+    "url": "/static/js/2.fc45028e.chunk.js"
+  },
+  {
+    "revision": "c1b3569d5b2f6f15fd9a",
+    "url": "/static/js/main.251e6542.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
